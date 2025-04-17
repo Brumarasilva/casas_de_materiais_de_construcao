01f5189db12bf5a6bcd6c32272c8f33cce2d695b
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-04-17 11:15:43 -0300

    adicionando navbar nas outras paginas do site, adicionando descrição, taxa de preço dentro dos produtos da pagina principal

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/index", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/index.html");
+});
 
 app.Run();
