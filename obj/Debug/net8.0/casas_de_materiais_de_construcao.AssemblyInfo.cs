6dbaab0f2ec5c2eba42e59c53bac739e3cd8fd11
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-06-18 10:36:44 -0300

    adicionando simbolo do carrinho e button comprar

--- obj/Debug/net8.0/casas_de_materiais_de_construcao.AssemblyInfo.cs
+++ obj/Debug/net8.0/casas_de_materiais_de_construcao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("casas_de_materiais_de_construcao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f5189db12bf5a6bcd6c32272c8f33cce2d695b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176262fe2eb4f57284ca29f5b658970525a4c425")]
 [assembly: System.Reflection.AssemblyProductAttribute("casas_de_materiais_de_construcao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("casas_de_materiais_de_construcao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
